e7524545a3596ab9d3429afb57eed61294b3a357
Mohammed Abd <mohqot61997@gmail.com>
2022-08-18 19:46:49 +0300

    =end shippings

--- resources/lang/en/admin/settings.php
+++ resources/lang/en/admin/settings.php
@@ -6,9 +6,7 @@
         'shipping method' => 'وسيلة التوصيل',
         'edit' => 'تعديل',
         'update' => 'تحديث',
-        'free shipping' => ' توصيل مجاني',
-        'inner shipping' => 'توصيل داخلي',
-        'outer shipping' => 'توصيل خارجي',
         'name' => 'الاسم',
         'shipping value' => 'قيمة التوصيل',
+        'status' => 'Status',
     ];
--- resources/lang/en/admin/sidebar.php
+++ resources/lang/en/admin/sidebar.php
@@ -3,8 +3,13 @@
     [
         'settings' => 'Settings',
         'shipping methods' => 'Shipping Methods',
+        'shipping method' => 'Shipping Method',
+        'edit' => 'Edit',
+        'update' => 'Update',
         'free shipping' => 'Free Shipping',
         'inner shipping' => 'Inner Shipping ',
         'outer shipping' => 'Outer Shipping',
+        'name' => 'Name',
+        'shipping value' => 'Shipping Value',
 
     ];
